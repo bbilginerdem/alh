d90f893882843abf931cbfc42e1117794550d406
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2024-12-02 23:41:05 +0300

    carried loading to nextjs functionality

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+export default function loading() {
+  return (
+    <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+      {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+      <div className="three-body">
+        <div className="three-body__dot" />
+        <div className="three-body__dot" />
+        <div className="three-body__dot" />
+      </div>
+    </div>
+  )
+}
