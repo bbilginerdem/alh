3cebc67c579715a2fac6026de61abee9e6273d3c
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2024-12-03 05:27:10 +0300

    biome rules for tailwind added
    class order & readme updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,12 @@
 export default function loading() {
-  return (
-    <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-      {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
-      <div className="three-body">
-        <div className="three-body__dot" />
-        <div className="three-body__dot" />
-        <div className="three-body__dot" />
-      </div>
-    </div>
-  )
+	return (
+		<div className="absolute z-[100] h-dvh w-screen flex-center overflow-hidden bg-violet-50">
+			{/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+			<div className="three-body">
+				<div className="three-body__dot" />
+				<div className="three-body__dot" />
+				<div className="three-body__dot" />
+			</div>
+		</div>
+	);
 }
