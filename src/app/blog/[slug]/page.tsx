8a417e66bf3059965efd6c113b5da31ecaa65ce6
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2025-06-24 20:42:23 +0300

    reorganized folder and styling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,23 +1,23 @@
 // app/blog/[slug]/page.tsx
 import { notFound } from "next/navigation";
-import { posts } from "@/data/posts";
+import { posts } from "@/lib/db/posts";
 
 export default async function BlogPost({
-	params,
+  params,
 }: {
-	params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string }>;
 }) {
-	const { slug } = await params;
-	const post = posts.find((post) => post.slug === slug);
+  const { slug } = await params;
+  const post = posts.find((post) => post.slug === slug);
 
-	if (!post) {
-		notFound();
-	}
+  if (!post) {
+    notFound();
+  }
 
-	return (
-		<div className="container mx-auto p-4">
-			<h1 className="mb-4 font-bold text-3xl">{post.title}</h1>
-			<p className="text-gray-700">{post.content}</p>
-		</div>
-	);
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="mb-4 font-bold text-3xl">{post.title}</h1>
+      <p className="text-gray-700">{post.content}</p>
+    </div>
+  );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,15 @@
 // app/blog/page.tsx
 import { BlogList } from "@/components/BlogList";
-import { posts } from "@/data/posts";
+import { posts } from "@/lib/db/posts";
 import CustomBlackLayout from "../../components/CustomBlackLayout";
 
 export default function BlogPage() {
-	return (
-		<CustomBlackLayout>
-			<div className="container mx-auto">
-				<h1 className="mb-8 font-bold text-3xl">Blog Posts</h1>
-				<BlogList posts={posts} />
-			</div>
-		</CustomBlackLayout>
-	);
+  return (
+    <CustomBlackLayout>
+      <div className="container mx-auto">
+        <h1 className="mb-8 font-bold text-3xl">Blog Posts</h1>
+        <BlogList posts={posts} />
+      </div>
+    </CustomBlackLayout>
+  );
 }
